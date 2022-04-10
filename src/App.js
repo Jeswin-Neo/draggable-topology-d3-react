f071da01610abaecab3542d43453585567d7ccb5
Armaan Mathew <armaan.mathew@neoito.com>
2022-04-10 13:52:56 +0530

    fixed some issues

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { useEffect } from 'react';
 import './App.css';
 import * as d3 from 'd3';
 import Graph from './data/graph';
+import AsyncLocalStorage from '@createnextapp/async-local-storage';
 
 const App = () => {
   const width = 1800;
-  const height = 900;
+  const height = 1000;
   useEffect(() => {
     const svg = d3
       .select('div')
@@ -20,35 +21,50 @@ const App = () => {
 
     let link = svg
       .append('g')
-      .attr('class', 'link')
+      .attr('class', 'line')
       .selectAll('line')
       .data(Graph.content)
       .enter()
       .append('line')
-      .attr('transform', 'translate(0,' - 60 + ')')
       .attr('id', (d, i) => 'rect' + i + '.link')
-      .attr('x1', function (d, i) {
-        return localStorage.getItem(`rect${i}.link.dx1`) !== null
-          ? localStorage.getItem(`rect${i}.link.dx1`)
-          : d.source.x + 70;
+      .attr('x1', (d, i) => {
+        try {
+          return localStorage.getItem(`rect${i}.link.dx1`) !== null
+            ? localStorage.getItem(`rect${i}.link.dx1`)
+            : d.source.x + 70;
+        } catch (err) {
+          console.log(err);
+        }
       })
-      .attr('y1', function (d, i) {
-        return localStorage.getItem(`rect${i}.link.dy1`) !== null
-          ? localStorage.getItem(`rect${i}.link.dy1`)
-          : d.source.y;
+      .attr('y1', (d, i) => {
+        try {
+          return localStorage.getItem(`rect${i}.link.dy1`) !== null
+            ? localStorage.getItem(`rect${i}.link.dy1`)
+            : d.source.y;
+        } catch (err) {
+          console.log(err);
+        }
       })
-      .attr('x2', function (d, i) {
-        return localStorage.getItem(`rect${i}.link.dx2`) !== null
-          ? localStorage.getItem(`rect${i}.link.dx2`)
-          : d.target.x + 50;
+      .attr('x2', (d, i) => {
+        try {
+          return localStorage.getItem(`rect${i}.link.dx2`) !== null
+            ? localStorage.getItem(`rect${i}.link.dx2`)
+            : d.target.x + 50;
+        } catch (err) {
+          console.log(err);
+        }
       })
-      .attr('y2', function (d, i) {
-        return localStorage.getItem(`rect${i}.link.dy2`) !== null
-          ? localStorage.getItem(`rect${i}.link.dy2`)
-          : d.target.y + 10;
+      .attr('y2', (d, i) => {
+        try {
+          return localStorage.getItem(`rect${i}.link.dy2`) !== null
+            ? localStorage.getItem(`rect${i}.link.dy2`)
+            : d.target.y + 10;
+        } catch (err) {
+          console.log(err);
+        }
       });
 
-    svg
+    const rect = svg
       .append('g')
       .attr('class', 'node')
       .selectAll('rect')
@@ -62,16 +78,33 @@ const App = () => {
       .style('fill', 'no')
       .style('rx', 6)
       .attr('x', function (d, i) {
-        return localStorage.getItem(`rect${i}.rect${i}.dx`) !== null
-          ? localStorage.getItem(`rect${i}.rect${i}.dx`)
-          : d.x;
+        if (
+          localStorage.getItem(`rect${i}.rect${i}.dx`) === null ||
+          undefined ||
+          ''
+        ) {
+          return d.x;
+        }
+        return localStorage.getItem(`rect${i}.rect${i}.dx`);
       })
       .attr('y', function (d, i) {
-        return localStorage.getItem(`rect${i}.rect${i}.dy`) !== null
-          ? localStorage.getItem(`rect${i}.rect${i}.dy`)
-          : d.y;
-      })
-      .call(d3.drag().on('drag', dragged));
+        if (
+          localStorage.getItem(`rect${i}.rect${i}.dy`) === null ||
+          undefined ||
+          ''
+        ) {
+          return d.y;
+        }
+        return localStorage.getItem(`rect${i}.rect${i}.dy`);
+      });
+
+    rect.call(
+      d3
+        .drag()
+        .on('start', dragStarted)
+        .on('drag', dragged)
+        .on('end', dragEnded),
+    );
 
     let text = svg
       .selectAll('text')
@@ -138,9 +171,17 @@ const App = () => {
       .attr('height', 55)
       .attr('href', (d) => d.google);
 
+    function dragStarted(d) {
+      console.log('drag started', d);
+      d3.select(this).raise().classed('active', true);
+    }
+
+    function dragEnded(d) {
+      console.log('drag ended', d);
+      d3.select(this).classed('active', false);
+    }
+
     async function dragged(event, d) {
-      //   console.log('event', event);
-      //   console.log('d', d);
       if (
         event.sourceEvent.target.id.length > 0 &&
         event.sourceEvent.target.id.split('.').length === 1
@@ -150,20 +191,37 @@ const App = () => {
         await setValForAtt(event.sourceEvent.target.id, d, event);
         d3.select(this).attr('x', d.x).attr('y', d.y);
 
-        link
-          .filter(function (l) {
-            return l.source === d;
-          })
-          .attr('x1', d.x + 70)
-          .attr('y1', d.y)
-          .filter(await setValForAtt('link1', d, event));
-        link
-          .filter(function (l) {
-            return l.target === d;
-          })
-          .attr('x2', d.x + 50)
-          .attr('y2', d.y + 10);
-        // .filter(await setValForAtt('link2', d, event));
+        if (event.sourceEvent.target.id === 'rect0') {
+          link
+            .filter(function (l) {
+              return l.source === d;
+            })
+            .attr('x1', d.x + 70)
+            .attr('y1', d.y)
+            .filter(await setValForAtt('link1', d, event));
+          link
+            .filter(function (l) {
+              return l.target === d;
+            })
+            .attr('x2', d.x + 50)
+            .attr('y2', d.y + 10);
+          // .filter(await setValForAtt('link2', d, event));
+        } else {
+          link
+            .filter(function (l) {
+              return l.source === d;
+            })
+            .attr('x1', d.x + 70)
+            .attr('y1', d.y)
+            .filter(await setValForAtt('link1', d, event));
+          link
+            .filter(function (l) {
+              return l.target === d;
+            })
+            .attr('x2', d.x + 50)
+            .attr('y2', d.y + 10);
+          //   .filter(await setValForAtt('link2', d, event));
+        }
 
         text
           .filter((t) => t.source === d)
@@ -184,60 +242,64 @@ const App = () => {
     }
 
     async function setValForAtt(type, d, event) {
-      if (type === 'link1') {
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.link' + '.dx1',
-          d.x + 70,
-        );
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.link' + '.dy1',
-          d.y,
-        );
-      } else if (type === 'link2') {
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.link' + '.dx2',
-          d.x + 50,
-        );
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.link' + '.dy2',
-          d.y + 10,
-        );
-      } else if (type === 'text') {
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dx',
-          d.x + 40,
-        );
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dy',
-          d.y + 20,
-        );
-      } else if (type === 'icons') {
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dx',
-          d.x + 2,
-        );
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dy',
-          d.y - 2,
-        );
-      } else if (type === 'googleImg') {
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dx',
-          d.x + 85,
-        );
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dy',
-          d.y + 40,
-        );
-      } else {
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dx',
-          d.x,
-        );
-        localStorage.setItem(
-          event.sourceEvent.target.id + '.' + type + '.dy',
-          d.y,
-        );
+      try {
+        if (type === 'link1') {
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.link' + '.dx1',
+            d.x + 70,
+          );
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.link' + '.dy1',
+            d.y,
+          );
+        } else if (type === 'link2') {
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.link' + '.dx2',
+            d.x + 50,
+          );
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.link' + '.dy2',
+            d.y + 10,
+          );
+        } else if (type === 'text') {
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dx',
+            d.x + 40,
+          );
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dy',
+            d.y + 20,
+          );
+        } else if (type === 'icons') {
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dx',
+            d.x + 2,
+          );
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dy',
+            d.y - 2,
+          );
+        } else if (type === 'googleImg') {
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dx',
+            d.x + 85,
+          );
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dy',
+            d.y + 40,
+          );
+        } else {
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dx',
+            d.x,
+          );
+          await AsyncLocalStorage.setItem(
+            event.sourceEvent.target.id + '.' + type + '.dy',
+            d.y,
+          );
+        }
+      } catch (err) {
+        console.log(err);
       }
       return true;
     }
--- src/data/graph.js
+++ src/data/graph.js
@@ -1,8 +1,8 @@
-import GoogleImg from "../images/google.png";
-import BlackIcon from "../images/black.png";
-import GreenIcon from "../images/green.png";
+import GoogleImg from '../images/google.png';
+import BlackIcon from '../images/black.png';
+import GreenIcon from '../images/green.png';
 
-export default {
+const Graph = {
   nodes: [
     { x: 400, y: 175 },
     { x: 200, y: 500 },
@@ -13,49 +13,55 @@ export default {
   ],
   content: [
     {
+      id: 1,
       image: BlackIcon,
       google: GoogleImg,
-      text: "Google",
+      text: 'Google',
       x: 400,
       y: 175,
       source: 0,
       target: 0,
     },
     {
+      id: 2,
       image: GreenIcon,
-      text: "New York-Sites",
+      text: 'New York-Sites',
       x: 200,
       y: 500,
       source: 1,
       target: 0,
     },
     {
+      id: 3,
       image: GreenIcon,
-      text: "New Jersey-Sites",
+      text: 'New Jersey-Sites',
       x: 500,
       y: 500,
       source: 2,
       target: 0,
     },
     {
+      id: 4,
       image: GreenIcon,
-      text: "Vermont-Sites",
+      text: 'Vermont-Sites',
       x: 800,
       y: 500,
       source: 3,
       target: 0,
     },
     {
+      id: 5,
       image: GreenIcon,
-      text: "Massachusetts-Sites",
+      text: 'Massachusetts-Sites',
       x: 1100,
       y: 500,
       source: 4,
       target: 0,
     },
     {
+      id: 6,
       image: GreenIcon,
-      text: "California-Sites",
+      text: 'California-Sites',
       x: 1400,
       y: 500,
       source: 5,
@@ -63,3 +69,4 @@ export default {
     },
   ],
 };
+export default Graph;
